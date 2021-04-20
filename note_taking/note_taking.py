918da8dc23d99a1eee962f6af23361216f33539a
Hassan <hassanaziz0012@gmail.com>
2021-04-20 19:24:24 +0500

    Added proper documentation and comments to code

--- note_taking/note_taking.py
+++ note_taking/note_taking.py
@@ -1,14 +1,16 @@
 # Contains the commands for the note-taking features of the bot.
 import sqlite3
+from typing import List, Tuple
 import discord
 from discord.ext import commands
 
 
 class Database:
+    "This class contains all the methods that involve interacting and working with the database. The bot commands simply call these methods to perform operations on the database."
 
     @staticmethod
-    def create_connection(db_file=r'database\notes.db'):
-        "Create a database connection to a SQLite database"
+    def create_connection(db_file=r'database\notes.db') -> sqlite3.Connection:
+        "Create a database connection to a SQLite database. Returns an sqlite3.Connection object."
         conn = None
         try:
             conn = sqlite3.connect(db_file)
@@ -20,7 +22,11 @@ class Database:
                 return conn
 
     @staticmethod
-    def create_required_tables(conn):
+    def create_required_tables(conn: sqlite3.Connection):
+        """
+        Creates a table called 'notes' in the database. Takes in a 'sqlite.Connection' object and executes the SQL code to create the necessary tables.
+        This function is automatically called as soon as a connection is created, in the Database.create_connection() function.
+        """
         try:
             c = conn.cursor()
 
@@ -38,7 +44,11 @@ class Database:
             print(e)
 
     @staticmethod
-    def check_notes(user: str):
+    def check_notes(user: str) -> List:
+        """
+        Retrieves the 'notes' from the database which belong to the given user and returns them as a List object.
+        param <user>: A string that contains the User's name and discord tag, for example Hassan#3557.
+        """
         try:
             conn = Database.create_connection()
 
@@ -53,23 +63,35 @@ class Database:
             print(e)
 
     @staticmethod
-    def read_note(user: str, title: str):
+    def read_note(user: str, title: str) -> Tuple:
+        """
+        Returns a 'note' tuple from the database which matches the given parameters.
+
+        param <user>: A string that contains the User's name and discord tag, for example Hassan#3557.
+        param <title>: A string that uniquely identifies the note. Each note has a title.
+        """
         try:
             conn = Database.create_connection()
 
             c = conn.cursor()
             sql_code = f"""SELECT * FROM notes WHERE user=? AND title=?"""
-            c.execute(sql_code, (user,title))
+            c.execute(sql_code, (user, title))
 
             note = c.fetchone()
             return note
 
         except sqlite3.Error as e:
             print(e)
-        
 
     @staticmethod
     def add_note(user: str, title: str, content: str):
+        """
+        Creates a 'note' in the database and returns the Row ID of that note.
+
+        param <user>: A string that contains the User's name and discord tag, for example Hassan#3557.
+        param <title>: A string that uniquely identifies the note. Each note has a title.
+        param <content>: A string that contains all the content in the note.
+        """
         try:
             conn = Database.create_connection()
             c = conn.cursor()
@@ -86,6 +108,12 @@ class Database:
 
     @staticmethod
     def remove_note(title: str, user: str):
+        """
+        Removes a 'note' from the database.
+
+        param <title>: A string that uniquely identifies the note. Each note has a title.
+        param <user>: A string that contains the User's name and discord tag, for example Hassan#3557.
+        """
         try:
             conn = Database.create_connection()
             c = conn.cursor()
