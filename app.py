f48b5c2185122a665d81c52c94f6d573bbeb0af2
Hassan <hassanaziz0012@gmail.com>
2022-02-24 20:58:45 +0500

    Set up .copy_urls command

--- app.py
+++ app.py
@@ -112,6 +112,7 @@ async def download_attachments(ctx, channel: discord.TextChannel):
 
 @client.command()
 async def remove_urls(ctx):
+    await ctx.send("Scanning server contents. May take a while...")
     for channel in ctx.guild.channels:
         if isinstance(channel, discord.TextChannel):
             async for message in channel.history(limit=1000):
@@ -119,16 +120,29 @@ async def remove_urls(ctx):
                 match = re.search(regex, message.content)
                 if match:
                     await message.delete()
-    print("Command finished.")
+    await ctx.send("Command finished.")
+
+@client.command()
+async def copy_urls(ctx):
+    await ctx.send("Scanning server contents. May take a while...")
+    for channel in ctx.guild.channels:
+        if isinstance(channel, discord.TextChannel):
+            async for message in channel.history(limit=1000):
+                regex = r"[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"
+                match = re.search(regex, message.content)
+                if match:
+                    await ctx.send(message.content)
+    await ctx.send("Command finished.")
 
 @client.command()
 async def remove_text(ctx, text: str):
+    await ctx.send("Scanning server contents. May take a while...")
     for channel in ctx.guild.channels:
         if isinstance(channel, discord.TextChannel):
             async for message in channel.history(limit=1000):
                 if text in message.content:
                     await message.delete()
-    print("Command finished.")
+    await ctx.send("Command finished.")
 
 @client.command()
 async def clear(ctx, amount: int = 5):
